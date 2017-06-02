e83d5fff4bfa8973f3a8b0e63e0d974cc351e508
ascuns <freethinkel@gmail.com>
2017-06-02 15:28:17 +0300

    сайт готов

--- contact.php
+++ contact.php
@@ -1,7 +1,20 @@
-<?php
-
-$email = htmlspecialchars('email');
-echo $email;
-echo "It's works";
-
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'freethinkel@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Заявка с сайта'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p> 
+                        <p>Комментарий: '.$_POST['comments'].'</p>                       
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
 ?>
